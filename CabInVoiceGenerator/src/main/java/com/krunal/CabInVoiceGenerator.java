9b26f0cbe7756c5b028a38e9e2ab41563c5f2e69
kunallad54 <kunallad5473@gmail.com>
2021-07-13 18:42:08 +0530

    Updated Master

--- CabInVoiceGenerator/src/main/java/com/krunal/CabInVoiceGenerator.java
+++ CabInVoiceGenerator/src/main/java/com/krunal/CabInVoiceGenerator.java
@@ -1,6 +1,23 @@
 /****************************************************************************************************
  *
  * Purpose :
+ *  UC1 : Given Distance and Time ,the invoice generator should return total fare of the journey
+ *        COST --> Rs. 10 per kilometer , Rs. 1 per minute
+ *                 Minimum Fare = 5 Rs.
+ *
+ *  UC 2 : The InVoice Generator Should Now Take Multiple Rides,and Calculate Total Aggregate
+ *         for all.
+ *
+ *  UC 3 : Enhanced Invoice :
+ *         The invoice generator should now return the following as a part of invoice :
+ *         - Total Number of Rides
+ *         - Total Fare
+ *        - Average Fare per Ride
+ *
+ *  UC 4 : InVoice Service :
+ *         Given an User ID,The Invoice Service gets the list of rides from
+ *         Ride Repository,and returns an invoice
+ *
  *  UC 5 : Premium Rides (Bonus)
  *         - The Cab Agency now supports two types of rides normal rides and premium rides
  *         1. Normal Rides - Rs. 10 per km, Rs. 1 per min, Minimum Fare - Rs. 5
